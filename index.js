961f13254e7aa0024922699dfe8f3b66c0d405e4
JiaEn <sy258794@gmail.com>
2026-09-02 15:36:08 +0800

    99

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ app.get("/api/products", async (req, res) => {
   try {
     const pool = await sql.connect(config);
 
-    const result = await pool.request().query("SELECT TOP 1* FROM dbo.product");
+    const result = await pool
+      .request()
+      .query("SELECT TOP 5 * FROM dbo.product");
 
     res.json(result.recordset);
   } catch (err) {
